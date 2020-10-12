077c0b89ffc0b59af6bb5e71570cda223a10c232
Sam Stahl <sstahl2@depaul.edu>
2020-10-12 13:44:20 -0500

    created rules and about pages. updates index with high score tables. added url routes to app

--- app.py
+++ app.py
@@ -24,6 +24,14 @@ name = ''
 def index():
 	return render_template('index.html')
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/rules')
+def rules():
+	return render_template('rules.html')
+
 #test pusher trigger
 @app.route('/index')
 def indexPy():
